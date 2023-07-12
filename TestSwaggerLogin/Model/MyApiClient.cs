6530f5cb266b1475b828a684fab7e625b9baf958
VitaliiKrab <palochkin99@gmail.com>
2023-07-12 16:49:00 +0300

    +

---
+++ TestSwaggerLogin/Model/MyApiClient.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TestSwaggerLogin.Model
+{
+    public class MyApiClient
+    {
+        private readonly HttpClient _client;
+
+        public MyApiClient()
+        {
+            _client = new HttpClient();
+        }
+        
+        public async Task<string> PostLogin(string requestUri)
+        {
+            string responseData;
+            HttpResponseMessage response;
+            
+            using (HttpClient client = new HttpClient()) {
+                using (response = await client.PostAsync(requestUri, null)) {
+                    responseData = await response.Content.ReadAsStringAsync();
+                }
+            }
+            MessageBox.Show(response.StatusCode.ToString(), "Result", MessageBoxButton.OK, MessageBoxImage.Information);
+            
+            return responseData;
+        }
+    }
+}
---
+++ TestSwaggerLogin/View/Login.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows;
+using TestSwaggerLogin.Model;
+
+namespace TestSwaggerLogin.View
+{
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            MyApiClient apiClient = new MyApiClient();
+            var res = apiClient.PostLogin("https://traceavit-pro.azurewebsites.net/api/user/login/" + tbLogin.Text);
+        }
+    }
+    
+}
+
